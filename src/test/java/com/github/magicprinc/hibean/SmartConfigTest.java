dab56fd4d669fd34509ae9119b6c6f659f791628
A.Fink <andrey.fink@platform-s.com>
2024-04-13 21:13:29 +0300

    SmartConfig works OK with SmallRyeConfig

--- src/test/java/com/github/magicprinc/hibean/SmartConfigTest.java
+++ src/test/java/com/github/magicprinc/hibean/SmartConfigTest.java
@@ -32,4 +32,22 @@ class SmartConfigTest {
 	void create () {
 		assertInstanceOf(SmartConfig.SmartConfigSysEnv.class, SmartConfig.of(new Properties()));
 	}
+
+//	@Test
+//	void microprofile () {
+//		var ebeanConfig = new Properties();
+//		ebeanConfig.setProperty("a", "111");
+//		ebeanConfig.setProperty("foo.bar.Zoo", " xXx ");
+//
+//		var cfg = new SmartConfig.SmartConfigMicroprofile(new SmallRyeConfigProviderResolver().getConfig(), ebeanConfig);
+//
+//		//System.out.println(cfg.getPropertyNames());
+//
+//		assertTrue(cfg.getPropertyNames().size() > 10);
+//		assertEquals("111", cfg.getProperty("a"));
+//		assertEquals(" xXx ", cfg.getProperty("foo.bar.Zoo"));
+//		assertTrue(Objects.requireNonNull(cfg.getProperty("java.version")).length() > 3);
+//		assertFalse(Objects.requireNonNull(cfg.getProperty(System.getenv().keySet().iterator().next())).isEmpty());
+//		assertTrue(cfg.toString().startsWith("SmartConfigMicroprofile:io.smallrye.config.SmallRyeConfig@"));
+//	}
 }
