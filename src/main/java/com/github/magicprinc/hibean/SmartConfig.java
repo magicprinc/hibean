6f8353b368cbaa6bdba0a7f637cd8d1c81c28c25
A.Fink <andrey.fink@platform-s.com>
2024-04-13 21:03:17 +0300

    + SmartConfig

---
+++ src/main/java/com/github/magicprinc/hibean/SmartConfig.java
@@ -0,0 +1,109 @@
+package com.github.magicprinc.hibean;
+
+import lombok.NonNull;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
+
+/**
+ @see HikariEbeanDataSourcePool
+ @see io.avaje.config.Config
+ @see org.eclipse.microprofile.config.Config
+ */
+interface SmartConfig {
+
+	List<String> getPropertyNames ();
+
+	String getProperty (String name);
+
+
+	static SmartConfig of (@NonNull Properties ebeanConfig) {
+		try {
+			Config config = ConfigProvider.getConfig();
+			return new SmartConfigMicroprofile(config, ebeanConfig);
+		} catch (Throwable e){// NoClassDefFoundError | IllegalStateException
+			return new SmartConfigSysEnv(ebeanConfig);
+		}
+	}
+
+
+  class SmartConfigMicroprofile implements SmartConfig {
+		private final Config config;
+		private final Properties ebeanConfig;
+
+		public SmartConfigMicroprofile (@NonNull Config config, Properties ebeanConfig) {
+			this.config = config;  this.ebeanConfig = ebeanConfig;
+		}//new
+
+		@Override
+		public String toString () {
+			return "SmartConfigMicroprofile:"+config+":"+ebeanConfig;
+		}
+
+		@Override
+		@Nullable	public String getProperty (String name) {
+			var value = config.getOptionalValue(name, String.class);
+			if (value != null && value.isPresent()){
+				return value.get();
+			}
+			return ebeanConfig.getProperty(name);
+		}
+
+		@Override
+		public List<String> getPropertyNames () {
+			var names = new ArrayList<String>(ebeanConfig.size() + 127);
+
+			names.addAll(ebeanConfig.keySet().stream().filter(Objects::nonNull).map(Object::toString).toList());
+
+			config.getPropertyNames().forEach(names::add);
+
+			return names;
+		}
+	}//SmartConfigMicroprofile
+
+	class SmartConfigSysEnv implements SmartConfig {
+		private final Properties ebeanConfig;
+
+		public SmartConfigSysEnv (Properties ebeanConfig) {
+			this.ebeanConfig = ebeanConfig;
+		}//new
+
+		@Override
+		public String toString () {
+			return "SmartConfigSysEnv:"+ebeanConfig;
+		}
+
+		@Override
+		@Nullable	public String getProperty (String name) {
+			var value = System.getProperty(name);
+			if (value != null){
+				return value;
+			}
+
+			value = System.getenv(name);
+			if (value != null){
+				return value;
+			}
+
+			return ebeanConfig.getProperty(name);
+		}
+
+		@Override
+		public List<String> getPropertyNames () {
+			var names = new ArrayList<String>(System.getenv().size() + System.getProperties().size() + ebeanConfig.size());
+
+			names.addAll(ebeanConfig.keySet().stream().filter(Objects::nonNull).map(Object::toString).toList());
+
+			names.addAll(System.getenv().keySet());
+
+			names.addAll(System.getProperties().keySet().stream().filter(Objects::nonNull).map(Object::toString).toList());
+
+			return names;
+		}
+	}//SmartConfigSysEnv
+}
---
+++ src/test/java/com/github/magicprinc/hibean/SmartConfigTest.java
@@ -0,0 +1,35 @@
+package com.github.magicprinc.hibean;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ @see SmartConfig */
+class SmartConfigTest {
+	@Test
+	void sysEnv () {
+		var ebeanConfig = new Properties();
+		ebeanConfig.setProperty("a", "111");
+		ebeanConfig.setProperty("foo.bar.Zoo", " xXx ");
+
+		var cfg = new SmartConfig.SmartConfigSysEnv(ebeanConfig);
+
+		//System.out.println(cfg.getPropertyNames());
+
+		assertTrue(cfg.getPropertyNames().size() > 10);
+		assertEquals("111", cfg.getProperty("a"));
+		assertEquals(" xXx ", cfg.getProperty("foo.bar.Zoo"));
+		assertTrue(Objects.requireNonNull(cfg.getProperty("java.version")).length() > 3);
+		assertFalse(Objects.requireNonNull(cfg.getProperty(System.getenv().keySet().iterator().next())).isEmpty());
+		assertTrue(cfg.toString().startsWith("SmartConfigSysEnv:{"));
+	}
+
+	@Test
+	void create () {
+		assertInstanceOf(SmartConfig.SmartConfigSysEnv.class, SmartConfig.of(new Properties()));
+	}
+}
